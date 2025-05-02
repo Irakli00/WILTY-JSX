249c80808618574839a72085179df93709da406e
Irakli00 <iraklitsurtsumia@gmail.com>
2025-05-02 17:30:50 +0400

    wtf

--- back/app.py
+++ back/app.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO
 from flask_socketio import join_room, leave_room, send
 
 import eventlet
-eventlet.monkey_patch()  # Optional but helpful
+# eventlet.monkey_patch()  # Optional but helpful
 
 flask_app = create_app()
 # Enable CORS for the entire application
