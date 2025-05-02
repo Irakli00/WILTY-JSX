f9388c073306fbaba4a43f511a548d159d5ba47a
Irakli00 <iraklitsurtsumia@gmail.com>
2025-05-02 17:24:53 +0400

    --

--- front/src/pages/StartPage.jsx
+++ front/src/pages/StartPage.jsx
@@ -2,26 +2,58 @@ import { Link } from "react-router-dom";
 
 import Button from "../components/Button";
 import styles from "./StartPage.module.css"; //better to get it to its module
+import { useContext, useEffect } from "react";
+import { AppContext } from "../contexts/AppContext";
 
-import { v4 as uuidv4 } from "uuid";
+function StartPage({ dispatch, onJoinLobby }) {
+  const { useClientId } = useContext(AppContext);
 
-const id = uuidv4().slice(0, 10);
-
-function StartPage({ dispatch }) {
   return (
     <section className={styles.gameSection}>
       <div className={styles.gameContainer}>
+        {/* ------------------------------------------------ */}
+        {useEffect(() => {
+          let id = sessionStorage.getItem("clientId");
+          if (id) {
+            fetch("http://localhost:5000/lobby_manager", {
+              method: "POST",
+              body: JSON.stringify({
+                userId: id,
+              }),
+              headers: {
+                "Content-type": "application/json; charset=UTF-8",
+              },
+            })
+              .then((res) => res.json())
+              .then((data) => console.log(data))
+              .catch((error) => console.error("Error:", error));
+          }
+        }, [])}
+
+        {/* {useEffect(() => {
+          const socket = io("http://localhost:5000/");
+
+          socket.on("connect", () => {
+            console.log("Connected to server");
+          });
+
+          socket.emit("message_from_client", { userId: "aruar" });
+
+          return () => socket.disconnect();
+        }, [])} */}
+        {/* ------------------------------------------------ */}
+
         <Link
-          to={`/${id}`}
+          to={`/${useClientId()}`}
           className={styles.playBtn}
           onClick={() => dispatch()}
         >
           PLAY
         </Link>
         <div>
-          <Button className={styles.rulesBtn}>Join a game</Button>
-          {/* <Button className={styles.rulesBtn}>Rules</Button> */}
-          {/* <Button className={styles.detailsBtn}>?</Button> */}
+          <Link className={styles.rulesBtn} to={""} onClick={onJoinLobby}>
+            Join a game
+          </Link>
         </div>
       </div>
     </section>
