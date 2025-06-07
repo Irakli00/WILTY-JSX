188dfc113c22266ea4cfddf3ba39ebf0f2b4bca0
Irakli00 <iraklitsurtsumia@gmail.com>
2025-06-07 20:12:28 +0400

    some bg stuff

--- front/src/pages/StartPage.jsx
+++ front/src/pages/StartPage.jsx
@@ -6,31 +6,29 @@ import { useContext, useEffect } from "react";
 function StartPage({ onJoinLobby }) {
   const { useClientId } = useContext(AppContext);
 
+  // {
+  //   useEffect(() => {
+  //     let id = sessionStorage.getItem("clientId");
+  //     if (id) {
+  //       fetch("http://localhost:5000/lobby_manager", {
+  //         method: "POST",
+  //         body: JSON.stringify({
+  //           userId: id,
+  //         }),
+  //         headers: {
+  //           "Content-type": "application/json; charset=UTF-8",
+  //         },
+  //       });
+  //       // .then((res) => res.json())
+  //       // .then((data) => console.log(data))
+  //       // .catch((error) => console.error("Error:", error));
+  //     }
+  //   }, []);
+  // }
+
   return (
-    <section className="layout-container">
-      <div className="bg-secondary-blue p-[5dvh] flex flex-col justify-between gap-[5dvh] rounded-2xl">
-        {/* -------------------   why is lobby manager even neccessary?  ----------------------------- */}
-
-        {useEffect(() => {
-          let id = sessionStorage.getItem("clientId");
-          if (id) {
-            fetch("http://localhost:5000/lobby_manager", {
-              method: "POST",
-              body: JSON.stringify({
-                userId: id,
-              }),
-              headers: {
-                "Content-type": "application/json; charset=UTF-8",
-              },
-            });
-            // .then((res) => res.json())
-            // .then((data) => console.log(data))
-            // .catch((error) => console.error("Error:", error));
-          }
-        }, [])}
-
-        {/* ------------------------------------------------ */}
-
+    <section className="layout-container ">
+      <div className="bg-secondary-blue min-h-full p-[5dvh] flex flex-col justify-between gap-[5dvh] rounded-2xl">
         <Link
           to={`/lobby/${useClientId()}`}
           className="bg-dark-yellow p-[7dvh] w-full text-[5rem] font-semibold text-dark-blue text-center rounded ease-in-out duration-300 hover:bg-greenish hover:text-white-tint"
